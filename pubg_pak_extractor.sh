20b330da9ba1bb01b9f1e915dee087e196706cbb
LESSRECOIL <84198186+LESSRECOIL@users.noreply.github.com>
2021-05-14 22:34:19 +0530

    Add files via upload

---
+++ pubg_pak_extractor.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+
+
+
+ 
+    
+
+
+
+
+printf 'if u know something teach to others'
+
+
+
+echo "This script is uses to Install Pak Extractor tool in termux app"
+echo "please subscribe to my channel merwick gaming"
+pkg install x11-repo -y
+
+pkg install qemu-system-i386 -y
+
+pkg install qemu-user-i386 -y
